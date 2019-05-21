0d87abe56faabd49a6edb4c42cee7ab0b94d17ca
Grigory Temchenko <soid.exe@gmail.com>
2019-05-20 20:43:56 -0700

    class codes display fix

--- classes.js
+++ classes.js
@@ -182,7 +182,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     Object.keys(classCodes).forEach(function (k) {
         var opt = document.createElement('option');
-        opt.appendChild(document.createTextNode(classCodes[k] + " (" + k + ")"));
+        opt.appendChild(document.createTextNode(classCodes[k] + ((classCodes[k] == k) ? "" : (" (" + k + ")"))));
         opt.value = k;
         sel.appendChild(opt);
     });
