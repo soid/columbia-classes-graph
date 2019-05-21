f3b0cdc8e4db44ae70d607727ac5f6be7eb8a33e
Grigory Temchenko <soid.exe@gmail.com>
2019-05-20 19:27:40 -0700

    renamed file fixed

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm data/all-classes.json
-scrapy runspider spider/scrapper.py -o data/all-classes.json
+scrapy runspider spider/allclasses.py -o data/all-classes.json
 python3 spider/converter.py
