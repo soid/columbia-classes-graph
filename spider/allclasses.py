16d2693ba00b4b9e4db16633e0984a4cd31ffd11
Grigory Temchenko <soid.exe@gmail.com>
2019-05-23 22:32:45 -0700

    enabled caching for spider

--- spider/allclasses.py
+++ spider/allclasses.py
@@ -12,6 +12,9 @@ class ClassesSpider(scrapy.Spider):
         # taken from http://bulletin.columbia.edu/general-studies/undergraduates/courses/?term=3&pl=0&ph=10&college=GS
         'http://bulletin.columbia.edu/ribbit/index.cgi?page=shared-scopo-search.rjs&criteria=%7B%22department%22%3A%22%22%2C%22term%22%3A%223%22%2C%22level%22%3A%22%22%2C%22held%22%3A%22%22%2C%22begin%22%3A%22%22%2C%22end%22%3A%22%22%2C%22pl%22%3A%220%22%2C%22ph%22%3A%2210%22%2C%22keywords%22%3A%22%22%2C%22college%22%3A%22GS%22%7D',
     ]
+    custom_settings = {
+        'HTTPCACHE_ENABLED': True
+    }
 
     def parse(self, response):
         for course_html in response.css('results result description'):
