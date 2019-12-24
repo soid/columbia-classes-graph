24e6892a825d31965a9965d2b85de4bfafe87e89
Grigory Temchenko <soid.exe@gmail.com>
2019-12-23 19:45:31 -0500

    run.sh: ignore if data file doesn't exist

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-rm data/all-classes.json
+set -x
+
+rm data/all-classes.json || true
 scrapy runspider spider/allclasses.py --logfile spider.log -o data/all-classes.json
 python3 spider/converter.py
